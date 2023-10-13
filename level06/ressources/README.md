7e99ccce5bbd6447a25bbff52270e3b8531c5775
rpoder <rpoder@student.42.fr>
2023-10-13 17:09:54 +0200

    level06 began

---
+++ level06/ressources/README.md
@@ -0,0 +1,5 @@
+the \2 is the second grouped capture
+\e execute code in the remplacement parameter
+
+./level06.php /var/tmp/toupie.sh '");...'
+This is not con de tester juste avec des points
--- level06/ressources/level06.php
+++ level06/ressources/level06.php
@@ -2,18 +2,19 @@
 <?php
 	function y($m)
 	{
-		$m = preg_replace("/\./", " x ", $m);
-		$m = preg_replace("/@/", " y", $m);
+		$m = preg_replace_callback("/\./", " x ", $m);
+		$m = preg_replace_callback("/@/", " y", $m);
 		return $m;
 	}
 	function x($y, $z)
 	{
 		$a = file_get_contents($y);
-		$a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a);
-		$a = preg_replace("/\[/", "(", $a);
-		$a = preg_replace("/\]/", ")", $a);
+		$a = preg_replace_callback("/(\[x (.*)\])/e", "y(\"\\2\")", $a);
+		$a = preg_replace_callback("/\[/", "(", $a);
+		$a = preg_replace_callback("/\]/", ")", $a);
 		return $a;
 	}
 	$r = x($argv[1], $argv[2]);
 	print $r;
 ?>
+y("\2")
