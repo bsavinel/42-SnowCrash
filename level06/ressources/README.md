47eaf85f44361aa0faab2d3e53744c8b4e91aba8
rpoder <rpoder@student.42.fr>
2023-10-17 17:23:04 +0200

    level06 completed

--- level06/ressources/README.md
+++ level06/ressources/README.md
@@ -1,5 +1,8 @@
-the \2 is the second grouped capture
-\e execute code in the remplacement parameter
+We find a .php file
+We can see that the program takes two parameters, one stream and another one.
 
-./level06.php /var/tmp/toupie.sh '");...'
-This is not con de tester juste avec des points
+The file contains the following line.
+$a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a);
+
+We can see that this script uses the '/e' and the '\2' option wich mean that the 2nd part of the regex group is gonna be executed.
+We just have to replace the argument as follows [x {${exec(getflag)}}]
--- level06/ressources/level06.php
+++ level06/ressources/level06.php
@@ -2,16 +2,16 @@
 <?php
 	function y($m)
 	{
-		$m = preg_replace_callback("/\./", " x ", $m);
-		$m = preg_replace_callback("/@/", " y", $m);
+		$m = preg_replace("/\./", " x ", $m);
+		$m = preg_replace("/@/", " y", $m);
 		return $m;
 	}
 	function x($y, $z)
 	{
 		$a = file_get_contents($y);
-		$a = preg_replace_callback("/(\[x (.*)\])/e", "y(\"\\2\")", $a);
-		$a = preg_replace_callback("/\[/", "(", $a);
-		$a = preg_replace_callback("/\]/", ")", $a);
+		$a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a);
+		$a = preg_replace("/\[/", "(", $a);
+		$a = preg_replace("/\]/", ")", $a);
 		return $a;
 	}
 	$r = x($argv[1], $argv[2]);
