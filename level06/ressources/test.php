90b4b08c19bce39d0337080429b445c71556c1a8
rpoder <rpoder@student.42.fr>
2023-10-17 16:43:46 +0200

    test changed

--- level06/ressources/test.php
+++ level06/ressources/test.php
@@ -1,13 +1,14 @@
 #!/usr/bin/php
 <?php
-$a = "a";
-$string = '");shell_exec("echo coucou");';
-function y($m)
-{
-	print "coucou\n";
-	print $m;
-	print "\n";
-	return 1;
-}
-print preg_replace('/^(.*)/e', 'y("\\1")', $string);
+	$a = "\"phpinfo()\"";
+	$string = "${a}";
+
+	function y($m)
+	{
+		print "coucou\n";
+		print $m;
+		print "\n";
+		return 1;
+	}
+	print preg_replace('/^(.*)/e', "y(\"\\1\")", $string);
 ?>
