254efa6c40d7172ca963125899a28df97247070d
rpoder <rpoder@student.42.fr>
2023-10-17 20:11:35 +0200

    level08 done

--- level08/ressources/README.md
+++ level08/ressources/README.md
@@ -1,1 +1,7 @@
+We can see a executable file and a file named token.
+We don't have permissions to read token, but that's what we want ! So we are gonna use the executable to read it.
+With Ghidra we can see in that the executable reads a file and print his content.
+The problem is that there is a protection for files named "token", so we can't send our token file as parameter to read through it.
+To fix it we just did a symbolic link to access the file with another name.
+
 ln -sf /home/user/level08/token /var/tmp/truc
--- level08/ressources/prog.c
+++ level08/ressources/prog.c
@@ -5,8 +5,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/* WARNING: Unknown calling convention */
-
 int main(int argc, char **argv, char **envp) {
   int fd;
   int rc;
@@ -16,35 +14,29 @@ int main(int argc, char **argv, char **envp) {
 
   localGuard = *(int *)(in_GS_OFFSET + 0x14);
 
-  // Check if there is an input file specified
   if (argc == 1) {
     printf("%s [file to read]\n", *argv);
-    exit(1); // Exit if no file specified
+    exit(1);
   }
 
-  // Check for the presence of the "token" substring in the input file name
   char *tokenCheck = strstr((char *)argv[1], "token");
   if (tokenCheck != NULL) {
     printf("You may not access '%s'\n", argv[1]);
-    exit(1); // Exit if "token" is found in the file name
+    exit(1);
   }
 
-  // Attempt to open the specified file
   fd = open((char *)argv[1], O_RDONLY);
   if (fd == -1) {
     err(1, "Unable to open %s", argv[1]);
   }
 
-  // Read data from the file into a local buffer
   ssize_t bytesRead = read(fd, localBuffer, 1024);
   if (bytesRead == -1) {
     err(1, "Unable to read fd %d", fd);
   }
 
-  // Write the read data to standard output
   ssize_t bytesWritten = write(1, localBuffer, bytesRead);
 
-  // Check for stack smashing
   if (localGuard != *(int *)(in_GS_OFFSET + 0x14)) {
     __stack_chk_fail();
   }
